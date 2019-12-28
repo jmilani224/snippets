214cb6c1ca3f0ac08a6e0e8b998079b140a097f9
jmilani224 <jmilani224@gmail.com>
2019-12-28 01:06:01 -0500

    Update roman-to-dec.js

--- roman-to-dec.js
+++ roman-to-dec.js
@@ -7,12 +7,38 @@ const romToDec = (roman) => {
   const d = 500;
   const m = 1000;
 
-  let romanSplit = roman.split("");
+  const romanSplit = roman.toLowerCase().split("");
+  let length = roman.length;
+  let sum = eval(romanSplit[0]);
 
-  roman.forEach(iterate())
+  if (eval(romanSplit[0]) < eval(romanSplit[1])) {
+      sum = eval(romanSplit[1]) - eval(romanSplit[0]);
+      romanSplit[1] = 0;
+  };
 
+  for (index = length - 1; index >= 1; index--) {
+      if (eval(romanSplit[index]) <= eval(romanSplit[(index - 1)])) {
+        sum += eval(romanSplit[index]);
+    } else {
+        sum += eval(romanSplit[index]) - eval(romanSplit[index - 1]);
+      index--;
+    };
+  };
   return sum;
-}
+};
 
+console.log(romToDec('CMXCIX')); //999
+console.log(romToDec('IM')); //999
 
-console.log(romToDec('xiv'))
+/*
+1	I	11	XI	21	XXI
+2	II	12	XII	22	XXII
+3	III	13	XIII	23	XXIII
+4	IV	14	XIV	24	XXIV
+5	V	15	XV	25	XXV
+6	VI	16	XVI	26	XXVI
+7	VII	17	XVII	27	XXVII
+8	VIII	18	XVIII	28	XXVIII
+9	IX	19	XIX	29	XXIX
+10	X	20	XX	30	XXX
+*/
