454f032b99f491a8ebf5db9cab3ddcf2dd06117a
jmilani224 <jmilani224@gmail.com>
2019-12-28 00:59:35 -0500

    Add files via upload

---
+++ roman-to-dec.js
@@ -0,0 +1,18 @@
+const romToDec = (roman) => {
+  const i = 1;
+  const v = 5;
+  const x = 10;
+  const l = 50;
+  const c = 100;
+  const d = 500;
+  const m = 1000;
+
+  let romanSplit = roman.split("");
+
+  roman.forEach(iterate())
+
+  return sum;
+}
+
+
+console.log(romToDec('xiv'))
